a02a922ec13911822b71e362132e3f1406aa4560
franciscobressa <bressafrancisco@gmail.com>
2022-06-02 13:03:46 -0400

    footer smartphone

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -14,10 +14,19 @@ function Footer() {
         <Card className='footer-card'>
           <CardBody className='footer-card-body'>
             <CardTitle className='footer-card-title' tag="h5">
-              Mustang
+              <span className='d-none d-md-block'>
+                Mustang
+              </span>
+              <span className='d-md-none'>
+                O que são
+              </span>
             </CardTitle>
             <CardText className='footer-card-text'>
-            O Ford Mustang é um automóvel desportivo produzido pela Ford Motor Company. O carro foi apresentado ao público em 17 de abril de 1964 durante a New York World's Fair. O Mustang, apesar de ter sofrido grandes alterações ao longo dos anos é a mais antiga linha de automóveis da Ford.            </CardText>
+              <span className='d-none d-md-block'>O Ford Mustang é um automóvel desportivo produzido pela Ford Motor Company. O carro foi apresentado ao público em 17 de abril de 1964 durante a New York World's Fair. O Mustang, apesar de ter sofrido grandes alterações ao longo dos anos é a mais antiga linha de automóveis da Ford.</span>
+              <span className='d-md-none'>
+                A sigla SUV significa Sport Utility Vehicle -- ou seja, veículo utilitário esportivo. As SUVs costumam ter porte avantajado, além de interior espaçoso e possibilidade de trafegar dentro e fora da cidade.
+              </span>
+            </CardText>
             <Button>
               Ver Carros
             </Button>
